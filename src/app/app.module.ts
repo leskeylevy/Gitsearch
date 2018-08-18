7900dc1e08b2e15cc1ef20e78ddbe6973cd18991
Levy <leskeylevy@gmail.com>
2018-08-18 17:06:54 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { GithubRequestService } from './github-http/github-request.service'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "search", component: SearchComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +20,8 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [GithubRequestService],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  search$;
 
+  constructor(private http: HttpClient) { }
+
+  searchgh(input) {
+    this.http.get('https://api.github.com/search/user?q=' + input.value).subscribe(result => {
+      this.search$ = result.items;
+      console.log(search$)
+    })
+  }
   ngOnInit() {
   }
 
