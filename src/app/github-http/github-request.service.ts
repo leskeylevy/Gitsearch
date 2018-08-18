67565d24c44cf43e65aa7f3574611ea1c87815a2
Levy <leskeylevy@gmail.com>
2018-08-18 14:09:41 +0300

    generating repository class

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -38,6 +38,10 @@ export class GithubRequestService {
     })
     return promise
 
+    repositories(){
+      return this.http.get("https://api.github.com/users/leskeylevy/repositories?access_token=b182da05cda24004ea9e522d63544ac79defc478");
+    }
+
   }
 
 }
